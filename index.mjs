2e7fa9463f17a993a34feb6405a356cd3d8faae4
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-04-13 17:52:28 +0200

    Link stylesheets middleware

--- index.mjs
+++ index.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import {RouteHandler} from "./routes/RouteHandler.mjs";
 
+
 let app = new express();
 const PORT = 3000;
 app.set('view engine', 'ejs');
 app.use('/', RouteHandler.getRouter());
+app.use('/static/css', express.static('./static/css'));
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);

7c12ade485ecc0803c74b7b4bbbc5a7147a1e430
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-04-13 17:55:39 +0200

    Creating controllers empty functions

--- controllers/UsersController.mjs
+++ controllers/UsersController.mjs
@@ -3,4 +3,23 @@ export class UsersController {
     homeController(request, response) {
         response.render("home", {});
     }
+
+    registerController(request, response) {
+    }
+
+    loginController(request, response) {
+    }
+
+    getUserController(request, response) {
+    }
+
+    getUsersController(request, response) {
+    }
+
+    updateUserController(request, response) {
+    }
+
+    deleteUserController(request, response) {
+    }
+
 }
